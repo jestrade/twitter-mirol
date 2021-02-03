09de919d43fcca531c8656c819d49112288f8ed0
Gustavo Morales <gustavo.morales@gmail.com>
2021-02-03 11:17:48 -0800

    Add username to the token

--- api/middlewares/authentication/index.js
+++ api/middlewares/authentication/index.js
@@ -6,8 +6,9 @@ const authentication = (req, res, next) => {
     const token = req.headers["x-access-token"];
     try {
         const decoded = jwt.verify(token, config.jwtKey);
-        const id = decoded.id; /*extraer del token decodificado, el id del usuario*/
+        const { id, username } = decoded; /*extraer del token decodificado, el id y el username del usuario*/
         req.id = id; /*agregar al objeto request, el id del usuario recién obtenido*/
+        req.username = username; /*agregar al objeto request, el username del usuario recién obtenido*/
         next();
     } catch(e) {
         res.status(401).json(response(false, undefined, "No autorizado"));
